d30d6fabb41bb21011ee89f477e09078b26dbae1
De Souza Oliveira <sjsouza@indracompany.com>
2019-09-03 23:26:29 -0300

    Commit de termino de upload e download de arquivos

--- src/main/java/br/com/crudApiRest/controller/FileController.java
+++ src/main/java/br/com/crudApiRest/controller/FileController.java
@@ -4,13 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 import br.com.crudApiRest.data.vo.UploadFileResponseVO;
 import br.com.crudApiRest.services.FileStorageService;
@@ -21,6 +32,8 @@ import io.swagger.annotations.Api;
 @RequestMapping("/api/file")
 public class FileController {
 
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+	
 	@Autowired
 	private FileStorageService fileStorageService;
 	
@@ -43,6 +56,29 @@ public class FileController {
 				.stream()
 				.map(file -> uploadFile(file))
 				.collect(Collectors.toList());
+	}
+	
+	@GetMapping("/downloadFile/{fileName:.+}")
+	public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request){
+		Resource resource = fileStorageService.loadFileAsResources(fileName);
+		
+		String contentType = null;
+		
+		try {
+			
+			contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+			
+		} catch (Exception e) {
+			logger.info("Could not determine file type");
+		}
+		
+		if(contentType == null) {
+			contentType = "application/octet-stream";
+		}
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+				.body(resource);
 	}
 }
 	
--- src/main/java/br/com/crudApiRest/services/FileStorageService.java
+++ src/main/java/br/com/crudApiRest/services/FileStorageService.java
@@ -6,43 +6,61 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.crudApiRest.config.FileStorageConfig;
 import br.com.crudApiRest.execption.FileStorageException;
+import br.com.crudApiRest.execption.MyFileNotFoundExecption;
 
 @Service
 public class FileStorageService {
 
 	private final Path fileStorageLocation;
-		
+
 	@Autowired
 	public FileStorageService(FileStorageConfig fileStorageConfig) {
-		
+
 		this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
-		
+
 		try {
 			Files.createDirectories(this.fileStorageLocation);
 		} catch (Exception e) {
 			throw new FileStorageException("Cold not create the directory where the upload files will be stored", e);
 		}
 	}
-	
+
 	public String storeFile(MultipartFile file) {
 		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-		
+
 		try {
-			if(fileName.contains("..")) {
+			if (fileName.contains("..")) {
 				throw new FileStorageException("Sorry FileName contains invalid path suquence" + fileName);
 			}
-			
+
 			Path targetLocation = this.fileStorageLocation.resolve(fileName);
 			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 			return fileName;
 		} catch (Exception e) {
 			throw new FileStorageException("Cold not store file " + fileName + " Please try again", e);
+		}
+	}
+
+	public Resource loadFileAsResources(String fileName) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) {
+				return resource;
+			} else {
+				throw new MyFileNotFoundExecption("File note found" + fileName);
+			}
+		} catch (Exception e) {
+			throw new MyFileNotFoundExecption("File note found" + fileName, e);
 		}
 	}
 }
